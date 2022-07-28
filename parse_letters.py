081c53e036683ea0566c5dcee877d33404ed0bd1
Steve Kaser <skaser@archerpoint.com>
2022-07-28 13:29:36 -0400

    added ability to print text

--- parse_letters.py
+++ parse_letters.py
@@ -23,8 +23,23 @@ with open('letters.txt', 'r') as f:
                     raise ValueError(f'Uh-uh: {curr_letter.letter} {chars}')
                 curr_letter.rows.append(chars)
 
-for letter in letters:
-    if len(letter.rows) > 0:
-        print(letter.letter)
-        for row in letter.rows:
-            print(''.join(row))
+# for letter in letters:
+#     if len(letter.rows) > 0:
+#         print(letter.letter)
+#         for row in letter.rows:
+#             print(''.join(row))
+
+def print_text(text: str) -> None:
+    text_letters = [[] for t in text]
+    for i, char in enumerate(text):
+        letter = [l for l in letters if l.letter == char][0]
+        for k in range(len(letter.rows)):
+            text_letters[i].append(''.join(letter.rows[k]))
+
+    lines = ['' for r in range(len(text_letters[0]))]
+    for i, line in enumerate(lines):
+        for t in text_letters:
+            line += t[i]
+        print(line)
+
+print_text('back')
